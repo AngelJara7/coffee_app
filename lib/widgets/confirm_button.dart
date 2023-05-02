48f658462a83747c43aa592eeeddaf7c84d1e450
Angel Jaramillo <angeljara07@outlook.com>
2023-05-02 00:59:59 -0500

    personalizando carga al agregar productos a la lista de pedido

--- lib/widgets/confirm_button.dart
+++ lib/widgets/confirm_button.dart
@@ -22,10 +22,14 @@ class ConfirmButton extends StatelessWidget {
         color: const Color.fromRGBO(45, 45, 45, 1),
       ),
       child: loading
-      ? const CircularProgressIndicator.adaptive(
-        backgroundColor: Colors.white,
-        valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(45, 45, 45, 1)),
-        strokeWidth: 3,
+      ? const SizedBox(
+        width: 25,
+        height: 25,
+        child: CircularProgressIndicator.adaptive(
+          backgroundColor: Colors.white,
+          valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(45, 45, 45, 1)),
+          strokeWidth: 4,
+        ),
       )
       : Text(text.toString(), style: GoogleFonts.poppins(fontSize: 16, color: Colors.white,), textAlign: TextAlign.center,)
     );
